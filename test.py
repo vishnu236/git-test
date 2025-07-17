7a1b93553695782c3963dea66a220939f7f784d1
vishnu vardhan <vishnu.vardhanpunnam@dxc.com>
2025-07-17 19:40:31 +0530

    this is text file and code

---
+++ test.py
@@ -0,0 +1,7 @@
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+result = num1 + num2
+print("The sum is:", result)
